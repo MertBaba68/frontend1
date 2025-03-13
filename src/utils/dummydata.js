12a193b28667a79cf9ec24a170dfbf9590fddad9
xet1309 <xavier.tromp@student.hu.nl>
2025-03-13 23:36:20 +0100

    added dummydata to simulate backend call

---
+++ src/utils/dummydata.js
@@ -0,0 +1,97 @@
+export const getCategoriesDummy = async () => {
+    return
+
+    [
+        {
+            "id": "7a0e6b1b-92d4-4b56-9905-e8c6d57b2b5b",
+            "name": "Product 1",
+            "image": "",
+            "services": [
+                {
+                    "id": "ad0b7d97-c99e-4e6b-bd1a-53a440d7e9ff",
+                    "name": "Service 1",
+                    "description": "Beschrijving van service 1",
+                    "image": ""
+                },
+                {
+                    "id": "fb4e63bb-b290-4e62-8324-ef5fa6ad1f63",
+                    "name": "Service 2",
+                    "description": "Beschrijving van service 2",
+                    "image": ""
+                }
+            ]
+        },
+        {
+            "id": "84663b12-86c3-4906-832d-22f3896a2c27",
+            "name": "Product 2",
+            "image": "",
+            "services": [
+                {
+                    "id": "c9b19f6f-d681-4f7e-b424-7fe48fd1e741",
+                    "name": "Service 1",
+                    "description": "Beschrijving van service 1",
+                    "image": ""
+                },
+                {
+                    "id": "8999a33b-79e3-4e2c-bfc9-cfa7e0840e1e",
+                    "name": "Service 3",
+                    "description": "Beschrijving van service 3",
+                    "image": ""
+                }
+            ]
+        },
+        {
+            "id": "09fcdab3-cd7b-469b-80a3-b5ef69875611",
+            "name": "Product 3",
+            "image": "",
+            "services": [
+                {
+                    "id": "ab890dba-c42d-4e29-a4b2-b4187d6d85e7",
+                    "name": "Service 4",
+                    "description": "Beschrijving van service 4",
+                    "image": ""
+                }
+            ]
+        },
+        {
+            "id": "a5dce5ab-8cfb-4c50-926f-ef8b587d71d2",
+            "name": "Product 4",
+            "image": "",
+            "services": [
+                {
+                    "id": "7b547935-f4b7-45e4-a256-6c50fd55be47",
+                    "name": "Service 5",
+                    "description": "Beschrijving van service 5",
+                    "image": ""
+                }
+            ]
+        },
+        {
+            "id": "f3f2ab11-bf4e-4d38-a62f-957e03c101b0",
+            "name": "Product 5",
+            "image": "",
+            "services": [
+                {
+                    "id": "d70de0ed-41eb-47e9-b88e-bb3747a573ee",
+                    "name": "Service 2",
+                    "description": "Beschrijving van service 2",
+                    "image": ""
+                }
+            ]
+        },
+        {
+            "id": "4b1b98e0-5090-47ff-89cc-eac9f1c9eaf9",
+            "name": "Product 6",
+            "image": "",
+            "services": [
+                {
+                    "id": "6d7bdb9a-d7e5-4f9d-b869-fc45c8422f38",
+                    "name": "Service 3",
+                    "description": "Beschrijving van service 3",
+                    "image": ""
+                }
+            ]
+        }
+    ]
+}
+
