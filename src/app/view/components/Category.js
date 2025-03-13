68812a8bf6ac49c3f7b11abe092e9789bb33548e
xet1309 <xavier.tromp@student.hu.nl>
2025-03-14 00:14:38 +0100

    added classes to elements

--- src/app/view/components/Category.js
+++ src/app/view/components/Category.js
@@ -4,9 +4,13 @@ const Category = (data) => {
     console.log(data)
     return (
         <>
-            <img alt="Image of service" src={"placeholder-categories.png"}/>
-            <h2>Agrarische Sensor</h2>
-            <p>Slimme agrarische sensor: meet bodemgegevens, bespaar water, verhoog opbrengst en optimaliseer gewasgroei in real-time!</p>
+            <div class="category-container">
+                <img className="category-image" alt="Image of service" src={"placeholder-categories.png"}/>
+                <h2 className="category-title" >Agrarische Sensor</h2>
+                <p className="category-description" >Slimme agrarische sensor: meet bodemgegevens, bespaar water, verhoog opbrengst en optimaliseer
+                    gewasgroei in real-time!</p>
+            </div>
+
         </>
     )
 }
