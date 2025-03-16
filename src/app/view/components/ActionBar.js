dac5abb5149de8fb44af87455d6b6ab2149b8b75
xet1309 <xavier.tromp@student.hu.nl>
2025-03-16 19:09:24 +0100

    . added background image

--- src/app/view/components/ActionBar.js
+++ src/app/view/components/ActionBar.js
@@ -3,13 +3,15 @@ import "/src/styles/ActionBar.css"
 const ActionBar = (category) => {
     return (
         <>
-            <div className="actionbar-container" >
-                <h1 className="actionbar-heading" >Agriculture</h1>
-                <p className="actionbar-description" >Zoek hier naar uw toekomstige dienst</p>
+            <div className="actionbar-container">
+                <h1 className="actionbar-heading">Agriculture</h1>
+                <p className="actionbar-description">Zoek hier naar uw toekomstige dienst</p>
                 <input
                     className="actionbar-input"
+                    type="text"
+                    placeholder="Zoeken"
                 />
-                <p className="actionbar-filter" >Filter</p>
+                <p className="actionbar-filter">Filter</p>
             </div>
         </>
 
