9d7f619ae9e96d5fb52cdfbecb7385b51effbf55
xet1309 <xavier.tromp@student.hu.nl>
2025-03-16 19:41:01 +0100

    . styled page more

--- src/app/view/components/ServicePage.js
+++ src/app/view/components/ServicePage.js
@@ -1,19 +1,25 @@
-import {getCategories, getServicesFromCategorie} from "@/app/service/CategorieService";
 import Services from "@/app/view/components/Services";
-import "/src/styles/ServicePage.css"
 import ActionBar from "@/app/view/components/ActionBar";
-
+import {getServicesFromCategorie} from "@/app/service/CategorieService";
+import "/src/styles/ServicePage.css"
 
 const ServicePage = async () => {
-
-    const servicesFromCategory = await getServicesFromCategorie()
+    const servicesFromCategory = await getServicesFromCategorie();
 
     return (
         <div className="service-page-container">
-            <ActionBar/>
-            <Services data={servicesFromCategory}/>
-        </div>
-    )
-}
+            <div className="actionbar-container" >
+                <ActionBar />
+            </div>
 
-export default ServicePage
+            <div className="services-container">
+                <Services data={servicesFromCategory} />
+            </div>
+            <div className="contact-container">
+                <p>Contact soon...</p>
+            </div>
+        </div>
+    );
+};
+
+export default ServicePage;
