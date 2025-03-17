610b1c60783c72b07f682d3b252c184c9caa6b01
xet1309 <xavier.tromp@student.hu.nl>
2025-03-18 00:52:18 +0100

    . added Contactfrom fields

--- src/app/view/components/ContactForm.js
+++ src/app/view/components/ContactForm.js
@@ -6,6 +6,11 @@ const ContactForm = () => {
         <Card>
             <h1 className="contactform-heading">Enthousiast?</h1>
             <p className="contactform-description">Vul hier uw informatie in</p>
+            <input type="text"/>
+            <input type="email"/>
+            <input type="number"/>
+            <input type="text"/>
+            <textarea className="contactform-description"></textarea>
         </Card>
     );
 };
