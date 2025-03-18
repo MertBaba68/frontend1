d4aea1a7a697a71f0406914e63b00c727750fcb1
xet1309 <xavier.tromp@student.hu.nl>
2025-03-18 19:09:08 +0100

    created button and now using vodafone font

--- src/app/view/components/VodafoneButton.js
+++ src/app/view/components/VodafoneButton.js
@@ -1,9 +1,9 @@
 import "/src/styles/VodafoneButton.css"
 
-const VodafoneButton = ({ text }) => {
+const VodafoneButton = ({ children }) => {
     return (
         <div className="vodafonebutton-container" >
-            <button className="vodafonebutton-button">{text}</button>
+            <button className="vodafonebutton-button">{children}</button>
         </div>
     )
 }
