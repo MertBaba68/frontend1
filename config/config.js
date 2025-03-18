e96bc67f7d8c0f686d9ac705ef468069526ee5ab
xet1309 <xavier.tromp@student.hu.nl>
2025-03-18 17:07:42 +0100

    solution wich has to be replaced for env var, if we can fix problem

--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
-
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080"
+const API_BASE_URL = "http://localhost:8080"
 
 export default {
     API_BASE_URL,
