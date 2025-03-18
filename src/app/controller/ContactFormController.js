b036af5d005152937f2fc7201264422fc70a6cb8
xet1309 <xavier.tromp@student.hu.nl>
2025-03-18 23:48:07 +0100

    email validation  more secure

--- src/app/controller/ContactFormController.js
+++ src/app/controller/ContactFormController.js
@@ -44,9 +44,13 @@ const ContactFormController = ({ page }) => {
 
     const validateContactForm = () => {
 
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
         const validations = [
             { condition: kvkNummer === "", message: "kvk number" },
-            { condition: email === "" || !email.includes("@"), message: "email" },
+            { condition: email === "" ||
+                    emailRegex.test(email)
+                , message: "email" },
             { condition: phone === "" || phone.length !== 10, message: "phonenumber" },
             { condition: name === "", message: "name" },
             { condition: context === "", message: "context" },
