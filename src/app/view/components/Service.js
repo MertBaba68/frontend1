3a349d68201bf4b34022ebce6f6b2c215528236c
xet1309 <xavier.tromp@student.hu.nl>
2025-03-19 01:07:59 +0100

    for demo purposes added more placeholder photos

--- src/app/view/components/Service.js
+++ src/app/view/components/Service.js
@@ -6,16 +6,25 @@ const Service = ({ serviceData }) => {
         return <p>Loading service data...</p>;
     }
 
+    const getRandomImage = () => {
+        const images = [
+            "/placeholder-categories.png",
+            "/tracking industry (1).png",
+            "/utility industry (1).png"
+        ];
+        const randomIndex = Math.floor(Math.random() * images.length);
+        return images[randomIndex];
+    }
+
     return (
         <>
             <div className="service-container">
-                <img className="service-image" alt="Image of service" src={"/placeholder-categories.png"}/>
+                <img className="service-image" alt="Image of service" src={getRandomImage()} />
                 <h2 className="service-title" >{serviceData.name}</h2>
                 <p className="service-description" >{serviceData.description}</p>
             </div>
-
         </>
     )
 }
 
-export default Service
+export default Service;
