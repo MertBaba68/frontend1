567910d7fe8f3a13adeec3ac92714dc588a7f252
xet1309 <xavier.tromp@student.hu.nl>
2025-03-20 11:11:45 +0100

    for test purtposes keep image

--- src/app/view/components/Service.js
+++ src/app/view/components/Service.js
@@ -6,12 +6,25 @@ const Service = ({ serviceData }) => {
         return <p>Loading service data...</p>;
     }
 
+    const getRandomImage = () => {
+        const images = [
+            "/tracking industry (1).png",
+            "/utility industry (1).png"
+        ];
+        const randomIndex = Math.floor(Math.random() * images.length);
+        return images[randomIndex];
+    }
+
     return (
         <>
             <div className="service-container">
-                <img className="service-image" alt="Image of service" src={"data:image/png;base64,"+serviceData.headerImage} />
-                <h2 className="service-title" >{serviceData.name}</h2>
-                <p className="service-description" >{serviceData.description}</p>
+                <img className="service-image" alt="Image of service"
+                     src={getRandomImage()}/>
+
+                {/*<img className="service-image" alt="Image of service"*/}
+                {/*     src={"data:image/png;base64," + serviceData.headerImage}/>*/}
+                <h2 className="service-title">{serviceData.name}</h2>
+                <p className="service-description">{serviceData.description}</p>
             </div>
         </>
     )
