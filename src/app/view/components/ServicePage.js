5f2374dd317e7364ff4ea4147a28e9298bd480d4
xet1309 <xavier.tromp@student.hu.nl>
2025-03-20 23:30:42 +0100

    removed dummydata and slight adjustments to work with backend

--- src/app/view/components/ServicePage.js
+++ src/app/view/components/ServicePage.js
@@ -7,52 +7,12 @@ import ContactFormController from "@/app/controller-layer/ContactFormController"
 
 const ServicePage = ({ serviceData }) => {
 
-    const dummy = {
-        "about": [
-            {
-                "title": "Payoff",
-                "description": "Maak briljante werkplekbeslissingen. Haal nog meer uit je kantoorruimtes. Nimway van Sony is een complete suite van slimme kantoorsensoren, schermen en applicaties.",
-                "list": [
-                    "Voor medewerkers: virtuele assistentie voor soepele werkdagen.",
-                    "Voor kantoormanagers: ruimte-analyse om je werkplek te optimaliseren.",
-                    "Voor leidinggevenden: nieuwe zakelijke voordelen met een slim kantoor."
-                ]
-            },
-            {
-                "title": "Waardepropositie",
-                "description": "Nimway lost problemen op met betrekking tot inefficiënt gebruik van kantoorruimtes en het gebrek aan inzicht in werkplekbezetting."
-            },
-            {
-                "description": "Nimway biedt bedrijven de mogelijkheid om hun werkplekken effectiever te gebruiken en werknemers te ondersteunen in hun dagelijkse werkzaamheden.",
-                "img": "/tracking industry (1).png"
-            },
-            {
-                "title": "Oplossing",
-                "description": "Nimway is een complete suite van slimme kantoorsensoren, schermen en applicaties. Het systeem maakt gebruik van real-time sensoren en workplace experience tools om directe kantoorintelligentie te bieden.",
-                "list": [
-                    "Real-time sensoren voor live data.",
-                    "Workplace experience tools voor betere besluitvorming.",
-                    "Eenvoudige integratie met bestaande systemen."
-                ],
-                "img": "/utility industry (1).png"
-            },
-            {
-                "description": "Door Nimway te implementeren, kunnen bedrijven hun kantoorruimtes optimaliseren, wat leidt tot een efficiënter gebruik van middelen en een verbeterde werkplekervaring voor werknemers."
-            }
-        ]
-    };
-
-    const service = {
-        ...serviceData,
-        ...dummy
-    }
-
     return(
         <>
             <div className="background-image-container">
                 <div className="background-image-text" >
-                    <h1 className="background-image-header" >{service.name}</h1>
-                    <p className="background-image-description" >{service.description}</p>
+                    <h1 className="background-image-header" >{serviceData.name}</h1>
+                    <p className="background-image-description" >{serviceData.description}</p>
                 </div>
             </div>
 
@@ -60,16 +20,14 @@ const ServicePage = ({ serviceData }) => {
                 <PageContainer>
                     <Card className="service-page-about-container" >
                         {
-                            dummy.about.map((section, index) => {
+                            serviceData.about.map((section, index) => {
                                 return <AboutSection
                                     key={index}
                                     header={section.title}
                                     description={section.description}
-                                    img={section.img}
+                                    img={section.image}
                                     list={section.list}
-
                                 />
-
                             })
                         }
                     </Card>
