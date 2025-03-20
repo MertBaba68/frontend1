4c63c4960598561b094a5e1cb890265ffc9b9f2b
xet1309 <xavier.tromp@student.hu.nl>
2025-03-20 12:00:27 +0100

    can use service id as url

--- src/app/controller-layer/ServicePageController.js
+++ src/app/controller-layer/ServicePageController.js
@@ -1,14 +1,37 @@
 "use client"
 
 import {useParams} from "next/navigation";
+import {useEffect, useState} from "react";
+import {getService} from "@/app/service-layer/ServiceService";
 
 const ServicePageController = () => {
-    const { service } = useParams();
+    const { serviceId } = useParams();
 
+    const [error, setError] = useState(null);
+    const [serviceData, setServiceData] = useState(null);
 
+    useEffect(() => {
+        console.log(serviceId);
+
+        if (!serviceId) return;
+
+        const fetchService = async () => {
+            try {
+                setError(null);
+                const data = await getService(serviceId);
+                setServiceData(data);
+            } catch (error) {
+                console.error("Error fetching service: " + serviceId, error);
+                setError(error);
+                setServiceData(null);
+            }
+        }
+
+        fetchService();
+    },[serviceId]);
 
     return(
-        <p>{service}</p>
+        <p>{serviceId}</p>
     )
 }
 
