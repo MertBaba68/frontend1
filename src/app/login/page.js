e00626b9c1fd18bcb8ce6b8d76e2829dc09c4c2d
FabianHartman <fabian.hartman@student.hu.nl>
2025-03-28 10:39:59 +0100

    working navbar

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,7 @@
-"use client";
-
 import LoginPageController from "@/app/controller-layer/LoginPageController";
-import Navbar from "../view/components/navbar";
 
 export default function Home() {
     return <>
-    <Navbar></Navbar>
     <LoginPageController/>
     </>
 }
--- src/app/view/components/navbar.js
+++ src/app/view/components/navbar.js
@@ -1,19 +1,38 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link'
-import { authToken } from '@/app/utils/auth';
+import Link from 'next/link';
+import { authToken, removeAuthToken } from '@/app/utils/auth';
 
 const Navbar = () => {
-  const [authenticated, setAuthenticated] = useState(authToken() != "");
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const checkAuth = () => {
+    // Ensure this code runs only in the browser, next.js doesn't have access to the sessionStorage
+    if (typeof window !== "undefined") {
+      setAuthenticated(authToken() !== null);
+    }
+  }
 
   useEffect(() => {
-    setAuth(authToken());
+    checkAuth();
+
+    // Ensure this code runs only in the browser, next.js doesn't render the navbar before
+    if (typeof window !== "undefined") {
+      const navbarElement = document.querySelector(".navbar");
+      navbarElement.addEventListener("login", checkAuth);
+  
+      return () => {
+          navbarElement.removeEventListener("login", checkAuth);
+      };
+    }
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('authToken');
-    setAuthenticated(false); 
+    removeAuthToken();
+    setAuthenticated(false);
+
+    window.location = "/login";
   };
 
   return (
@@ -30,7 +49,6 @@ const Navbar = () => {
             <Link href="/contact">Contact</Link>
           </li>
 
-          {/* Conditional rendering for login/logout button */}
           {!authenticated ? (
             <li>
               <Link href="/login">Login</Link>
